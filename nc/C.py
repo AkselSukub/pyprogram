df8280697491e65f1efc76242775a2bc2d9a4de6
AkselSukub <aksel.sukubov@yandex.ru>
2023-09-20 19:32:24 +0300

    NewCommit4

--- nc/C.py
+++ nc/C.py
@@ -2,4 +2,6 @@ import random
 
 print("Hi")
 print("Угадай загаданное число")
-guesses_made = 3
+guesses_made = 0
+number = random.randint(1, 20)
+print('Отлично, {0}, я загадал число между 1 и 20. Сможешь угадать?')
