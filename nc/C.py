530a7b3e924a48d841992e52c5b15ac597ba157b
AkselSukub <aksel.sukubov@yandex.ru>
2023-09-20 19:38:08 +0300

    NewCommit6

--- nc/C.py
+++ nc/C.py
@@ -7,3 +7,4 @@ number = random.randint(1, 20)
 print('Отлично, {0}, я загадал число между 1 и 20. Сможешь угадать?')
 while guesses_made < 3:
     guess = int(input('Введи число: '))
+    guesses_made += 1
